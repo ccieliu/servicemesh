e4fa93ac28efe672cc7005333ee4c49c8a77b39e
lyx10516 <yuxuan.liu@uisee.com>
2021-09-03 17:31:33 +0800

    add header feature

--- web-app.py
+++ web-app.py
@@ -1,7 +1,7 @@
 '''
 Author: your name
 Date: 2021-09-02 13:39:32
-LastEditTime: 2021-09-02 17:39:23
+LastEditTime: 2021-09-03 17:17:23
 LastEditors: Please set LastEditors
 Description: In User Settings Edit
 FilePath: /mesh_demo/web.py
@@ -20,13 +20,19 @@ api = Api(app)
 
 class ResponseVersion(Resource):
     def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('namespace',location='headers')
+        args = parser.parse_args()
+        namespace=args['namespace']
+        metadata=[('namespace',namespace)]
 
         with grpc.insecure_channel('grpc-backend-svc:50051') as channel:
             stub = helloworld_pb2_grpc.GreeterStub(channel)
-            response = stub.SayHello(helloworld_pb2.HelloRequest(name='v1.0'))
+            response = stub.SayHello(helloworld_pb2.HelloRequest(name='v1.0'),metadata=metadata)
 
 
-        res = requests.get('http://http-backend-svc/').json()
+
+        res = requests.get('http://http-backend-svc/',headers={'namespace':namespace}).json()
         responseDic = {
             'web-version': os.environ['APP_VERSION'],
             'http-backend': {'hostname': res['hostname'],
